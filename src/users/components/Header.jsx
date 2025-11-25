9bb65556ebc2d2f76245f8d4cb273cabca615a55
ABHISHEK8289 <abhisheksabhi1116@gmail.com>
2025-11-25 12:59:20 +0530

    todaysection

--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars, FaFacebook, FaInstagram, FaUser, FaXTwitter } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
+Link
 
 function Header() {
+  const [listStatus,setListStatus]= useState(false)
+  const menuBtnClick=()=>{
+    setListStatus(!listStatus)
+  }
   return (
-    <div>Header</div>
+   <> 
+   {/* upper-part --tittle & login */}
+<div  className='grid grid-cols-3 p-3'>
+    {/* login tittle */}
+<div className='flex items-center'>
+      <img width={'70px'} height={'70px'} src="/public/logo.jpg" alt="" />
+      <h1 className='text-2xl font-bold  md:hidden'>BOOK-STORE</h1>
+</div>
+<div className=' md:flex justify-center items-center hidden'>
+      <h1 className='text-3xl font-bold'> BOOK-STORE</h1></div>
+<div className='md:flex justify-end items-center hidden'>
+<FaFacebook/>
+<FaInstagram className='mx-2'/>
+<FaXTwitter/>
+        <Link to={'/'} className='ms-3 border rounded py-2 hover:bg-black hover:bg-white flex items-center'> <FaUser className='me-2'/>Login</Link>
+
+
+</div>
+</div>
+{/* loweer-part */}
+
+<nav className='w-full p-2 bg-black text-white md:flex justify-center items-center '>
+
+  <div className='flex justify-between items-center text-2xl md:hidden'>
+
+    <button onClick={menuBtnClick}><FaBars/></button>
+    <Link to={'/login'} className='ms-4 border rounded px-2 py-1 hover:bg-white hover:text-black flex items-center'><FaUser className='me-1'/> </Link>
+  </div>
+  {/* ul-Links */}
+
+  <ul className={listStatus? "flex flex-col" : "md:flex justify-center items-center hidden" }>
+    <li className='md:mx-4 mt-3 md:mt-0'><Link to={'/'} >HOME</Link></li>
+    <li className='md:mx-4 my-3 md:my-0'><Link to={'/books'} >BOOKS</Link></li>
+    <li className='md:mx-4 mb-3 md:mb-0'><Link to={'/contact'} >CONTACT</Link></li>
+
+
+  </ul>
+</nav>
+   </>
   )
 }
 
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaSearch } from 'react-icons/fa'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Header/>
+    <div>
+      {/* landing part- search*/}
+      <div style={{height:'700px'}} className='flex justify-center items-center flex-col bg-[url(/public/background-image.png)] bg-cover text-white'>
+      <div style={{height:'700px' , backgroundColor:'rgba(0,0,0,5'}} className='w-full flex justify-center items-center flex-col'>
+        <h1 className='text-5xl font-bold mb-2'>WonderFul Gifts</h1>
+        <p>gifts your family  and friends a book</p>
+        {/* search */}
+        <div className='mt-9 flex'>
+          <input type="text" className='bg-white rounded-3xl text-black w-100 placeholder-gray-500 p-2' placeholder='Search a BOOKS HRER'  />
+          <button className='text-gray-500' style={{marginLeft:"-40px"}}><FaSearch/></button>
+        </div>
+      </div>
+       </div>
+      {/* new arribel */}
+      <section className='md:px-40 p-5 flex flex-col justify-center items-center'>
+        <h1 className='text-3xl font-bold'>NEW ARRIVALS</h1>
+        <h1 className='text-2xl'>Explore Our Latest Collections</h1>
+      </section>
+      {/* new auther */}
+      {/* testimoney */}
+    </div>
+    <Footer/>
+    </>
   )
 }
 
